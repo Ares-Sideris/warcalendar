3fa78b2b834e5594d90ae737196490d24958d5ef
Ares-Sideris <130211634+Ares-Sideris@users.noreply.github.com>
2025-07-26 16:37:12 +0300

    Remove unused or_ import

--- warcalendar_backend.py
+++ warcalendar_backend.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, Header
-from sqlalchemy import Column, Integer, String, DateTime, Table, ForeignKey, create_engine, and_, or_
+from sqlalchemy import Column, Integer, String, DateTime, Table, ForeignKey, create_engine, and_
 from sqlalchemy.orm import relationship, sessionmaker, declarative_base, Session
 from pydantic import BaseModel
 from datetime import datetime
